147a3820c2534c90a241f5178c6fe531eb1f97b3
Friederike <friederike@schertel.org>
2021-04-29 00:04:23 +0200

    eigene component war unnötig. brauch nur css

--- src/AndersUeberlegenButton.svelte
+++ src/AndersUeberlegenButton.svelte
@@ -1,8 +1,7 @@
 <script>
 	import { ablehnenUndNeuVorschlagen } from './Essen.js';
-	import Button from './Button.svelte';
 </script>
 
-	<Button on:click={ablehnenUndNeuVorschlagen}>
+	<button on:click={ablehnenUndNeuVorschlagen}>
 		Nee, was andreres
-	</Button>
+	</button>
--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,6 @@
 	import Ratlos from './Ratlos.svelte';
 	import MenueVorschlag from './MenueVorschlag.svelte';
 	import AndersUeberlegenButton from './AndersUeberlegenButton.svelte';
-	import Button from './Button.svelte';
 
 	let start = true;
 	function starte() {
@@ -16,7 +15,7 @@
 	{#if start}
 		<h1>Was essen wir heute?</h1>
 
-		<Button on:click={starte}>Sag's mir!</Button>
+		<button on:click={starte}>Sag's mir!</button>
 	{:else}
 		<Ratlos />
 		<MenueVorschlag />
